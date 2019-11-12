b170f38d8865b5f8b0302bbf5b6c525c5b07d019
Vu Tuan Anh <vtanh1905@gmail.com>
2019-11-12 14:30:17 +0700

    Config webpack basic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
-  return <>JSX</>;
+  return <>Webpack Practise</>;
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,48 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+
+// plugins
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // Kết nối HTML với file bundle.js
+const WebpackBar = require("webpackbar"); // Để vễ process bar
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // Tự động xóa các file build sau khi npm start
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js", // File sẽ được biên dịch
+
+  // Nơi file đươc build ra
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js"
   },
+
   module: {
     rules: [
+      // Convert ES6,7 => ES5
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
         }
       },
+      // Convert style, file css
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
+
+  // Setting PORT LINK HEADER ...
   devServer: {
-    port: 3000
+    port: 3000, // Set port cho server
+    noInfo: true // Không cho hiên message webpack khi run
   },
+
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
+      template: "./public/index.html"
+    }),
+    new WebpackBar(),
+    new CleanWebpackPlugin()
   ]
 };
